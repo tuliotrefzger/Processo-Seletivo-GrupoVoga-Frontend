11590cc63d2fbb6bf6f71ec23dc459d1610f667f
tuliotrefzger <tuliotrefzgermello@gmail.com>
2022-12-03 15:15:17 -0300

    🐛 fixing navbar bug

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -125,7 +125,7 @@ function Navbar() {
               variant="h5"
               noWrap
               component="a"
-              href=""
+              href="/"
               sx={{
                 mr: 2,
                 display: { xs: "flex", md: "none" },
